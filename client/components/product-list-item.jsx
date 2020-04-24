2964c9b34335bbc8baf22b6e1a2bdbd4fd527301
Mikayla Ruiz <mikaylarruiz@gamil.com>
2020-04-24 22:01:09 +0000

    Feature 4 completed view details front end

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
-function ProductListItem(props) {
+export default function ProductListItem(props) {
   const productImage = props.productImage;
   const productName = props.productName;
   const productPrice = props.productPrice;
   const productDescription = props.productDescription;
+  const productId = props.productId;
+  const setViewProp = props.setViewProp;
 
   return (
-    <div className="card col-3 m-2">
-      <img className="card-img-top cardImage" src={productImage}/>
+    <div className="card col-3 m-2" onClick={() => setViewProp('details', { productId: productId })}>
+      <img className="card-img-top" src={productImage} />
       <div className="card-body p-2">
         <h5 className="card-title">{productName}</h5>
         <p className="text-muted">{productPrice}</p>
@@ -17,5 +18,3 @@ function ProductListItem(props) {
     </div>
   );
 }
-
-export default ProductListItem;
--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -23,15 +23,18 @@ class ProductList extends React.Component {
 
   render() {
     const productsListArray = this.state.products;
+    const setViewProp = this.props.setViewProp;
     return (
       productsListArray.map(product => {
         return (
           <ProductListItem
             key={product.productId}
+            productId={product.productId}
             productImage={product.image}
             productName={product.name}
             productPrice={`$${product.price}`}
             productDescription={product.shortDescription}
+            setViewProp={setViewProp}
           />
         );
       })
